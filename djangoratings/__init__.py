cf2f33aeeba4a3f895e307ab94bc91a279158d57
Nathan Shafer <nate@torzo.com>
2014-06-26 15:20:51 -0700

    Increment version

--- djangoratings/__init__.py
+++ djangoratings/__init__.py
@@ -1,7 +1,7 @@
 import os.path
 import warnings
 
-__version__ = (0, 3, 9)
+__version__ = (0, 3, 10)
 
 def _get_git_revision(path):
     revision_file = os.path.join(path, 'refs', 'heads', 'master')
