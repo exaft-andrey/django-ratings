b42aaefe2db0b742b41d0e4705f172df6942ab23
Nathan Shafer <nate@torzo.com>
2014-06-26 12:46:43 -0700

    Changed to use settings.AUTH_USER_MODEL instead of get_user_model(), which can cause issues at import time

--- djangoratings/models.py
+++ djangoratings/models.py
@@ -1,10 +1,9 @@
 from datetime import datetime
+from django.conf import settings
 
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
-from django.contrib.auth import get_user_model
-User = get_user_model()
 
 try:
     from django.utils.timezone import now
@@ -18,7 +17,7 @@ class Vote(models.Model):
     object_id       = models.PositiveIntegerField()
     key             = models.CharField(max_length=32)
     score           = models.IntegerField()
-    user            = models.ForeignKey(User, blank=True, null=True, related_name="votes")
+    user            = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, related_name="votes")
     ip_address      = models.IPAddressField()
     cookie          = models.CharField(max_length=32, blank=True, null=True)
     date_added      = models.DateTimeField(default=now, editable=False)
@@ -56,7 +55,7 @@ class Score(models.Model):
     key             = models.CharField(max_length=32)
     score           = models.IntegerField()
     votes           = models.PositiveIntegerField()
-    
+
     content_object  = generic.GenericForeignKey()
 
     class Meta:
@@ -66,14 +65,14 @@ class Score(models.Model):
         return u"%s scored %s with %s votes" % (self.content_object, self.score, self.votes)
 
 class SimilarUser(models.Model):
-    from_user       = models.ForeignKey(User, related_name="similar_users")
-    to_user         = models.ForeignKey(User, related_name="similar_users_from")
+    from_user       = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="similar_users")
+    to_user         = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="similar_users_from")
     agrees          = models.PositiveIntegerField(default=0)
     disagrees       = models.PositiveIntegerField(default=0)
     exclude         = models.BooleanField(default=False)
-    
+
     objects         = SimilarUserManager()
-    
+
     class Meta:
         unique_together = (('from_user', 'to_user'),)
 
@@ -81,7 +80,7 @@ class SimilarUser(models.Model):
         print u"%s %s similar to %s" % (self.from_user, self.exclude and 'is not' or 'is', self.to_user)
 
 class IgnoredObject(models.Model):
-    user            = models.ForeignKey(User)
+    user            = models.ForeignKey(settings.AUTH_USER_MODEL)
     content_type    = models.ForeignKey(ContentType)
     object_id       = models.PositiveIntegerField()
     
